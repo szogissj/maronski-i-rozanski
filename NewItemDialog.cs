5c061a19e713ebb8ac68e0687b2e3c3a8e05df67
szogissj <szogissj@gmail.com>
2016-12-11 17:55:30 +0100

    Add files via upload

---
+++ NewItemDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Commander.Dialogs
+{
+    public partial class NewItemDialog : Form
+    {
+
+        public string Name { get; private set; }
+
+        public NewItemDialog()
+        {
+            InitializeComponent();
+        }
+
+        public NewItemDialog(string title)
+        {
+            InitializeComponent();
+
+            Text = title;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(folderName.Text))
+            {
+                MessageBox.Show("Nazwa nie może być pusta", "Podaj nazwę jeszcze raz", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            DialogResult = DialogResult.OK;
+            Name = folderName.Text;
+            Dispose();
+        }
+    }
+}
