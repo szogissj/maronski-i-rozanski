0a643f7a5e7d9b0531e66a8a28a13ae5b55ebef3
szogissj <szogissj@gmail.com>
2016-12-11 17:53:57 +0100

    Add files via upload
    zawartosc folderu Controlers

---
+++ IControler.cs
@@ -0,0 +1,14 @@
+﻿using Commander.Models;
+using Commander.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Commander.Controlers
+{
+    public interface IControler
+    {
+
+    }
+}
